e011ca7af1c4e47e40c0a36737807ba77f7771db
Srini Vasudevan <srini@gofundraise.com>
2019-11-22 16:30:58 +1100

    added snk for test project

--- src/Amazon.SQS.ExtendedClient/Properties/AssemblyInfo.cs
+++ src/Amazon.SQS.ExtendedClient/Properties/AssemblyInfo.cs
@@ -9,4 +9,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("38832a23-5aea-4856-991e-81ed4082bb67")]
 
-[assembly: InternalsVisibleTo("Amazon.SQS.ExtendedClient.Tests")]
+[assembly: InternalsVisibleTo("Amazon.SQS.ExtendedClient.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010079bef7c7d19227117010c6ba25f4db5470b11e81a9f095c405e8ccf075c840dac31a87e6a950d3f9e1c0c70e905e32f99ee7cbc71207643673a192d71b5293f6d7dfa1b01cdf5f401cf4154d789c7532669eda8403a2073c1aa2b46d9e9fcc5fdd17610c52ce384583de00d5545e3c26d09eebda30c537af02a967a286429eb1")]
